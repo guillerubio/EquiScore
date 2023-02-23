6c41323672cbf4149f0546bf4da505cd245c3f2f
guillerubio <rubi.o@icloud.com>
2023-02-23 21:55:18 +0100

    Img filter, not valid section added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Made by William Rubio on February 2023
+# Made by Guillermo (William) Rubio on February 2023
 import tensorflow as tf
 import os
 
@@ -6,4 +6,27 @@ import os
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
+
+import imghdr
+import cv2
+
+data_dir = "data/BCS"
+image_exts = ["jpeg", "jpg", "bmp", "png"]
+
+# Database filter
+for image_class in os.listdir(data_dir):
+    if image_class != ".DS_Store":
+        for image in os.listdir(os.path.join(data_dir, image_class)):
+            image_path = os.path.join(data_dir, image_class, image)
+            try:
+                img = cv2.imread(image_path)
+                tip = imghdr.what(image_path)
+                if tip not in image_exts:
+                    print('Image not in ext list {}'.format(image_path))
+                    os.remove(image_path)
+            except Exception as e:
+                print('Issue with image {}'.format(image_path))
+                # os.remove(image_path)
+
+
 
