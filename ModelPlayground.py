8c614f19e7a9d7ef4aa31bb7058befae7db77767
guillerubio <rubi.o@icloud.com>
2023-02-27 21:10:32 +0100

    Working well, next step: GUI

--- ModelPlayground.py
+++ ModelPlayground.py
@@ -5,24 +5,34 @@ from keras.models import load_model
 import os
 import cv2
 
-model = load_model(os.path.join("models", "es_dlm_00.h5"))
-img = cv2.imread("test.jpg")
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.show()
-resize = tf.image.resize(img, (256, 256))
-ans = model.predict(np.expand_dims(resize / 255, 0))
-possibilityArray = ans[0]
-print("done")
-print("ans = " + str(ans))
-print(ans[0][0])
-bigX = 0
-pos = 1
-posAns = -1
-for x in possibilityArray:
-    if(x > bigX):
-        bigX = x
-        posAns = pos
-    pos = pos+1
-print("Henneke score = " + str(posAns))
+
+def prediction(model_to_use, img_path):
+    model = load_model(os.path.join("models", img_path))
+    img = cv2.imread(model_to_use)
+    resize = tf.image.resize(img, (256, 256))
+    ans = model.predict(np.expand_dims(resize / 255, 0))
+    return str(ans)
 
 
+
+class ModelPlayground:
+
+    model = load_model(os.path.join("models", "es_dlm_00.h5"))
+    img = cv2.imread("test.jpg")
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.show()
+    resize = tf.image.resize(img, (256, 256))
+    ans = model.predict(np.expand_dims(resize / 255, 0))
+    possibilityArray = ans[0]
+    print("done")
+    print("ans = " + str(ans))
+    print(ans[0][0])
+    bigX = 0
+    pos = 1
+    posAns = -1
+    for x in possibilityArray:
+        if (x > bigX):
+            bigX = x
+            posAns = pos
+        pos = pos + 1
+    print("Henneke score = " + str(posAns))
